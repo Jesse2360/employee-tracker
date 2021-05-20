28e8efca52872262a18727f89233a35e30d509e2
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-05-20 01:02:05 -0500

    function for cnonecting

---
+++ server.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'intramuscular10',
+    database: 'employee_DB',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    connection.end();
+});                                                                                                                                             
