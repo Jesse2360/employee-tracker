14d2b2272f0402a4fe6782530a79c760290b07cd
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-05-22 00:16:19 -0500

    added switch statement

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const express = require('express');
-const fs = require('fs');
+const cTable = require('console.table');
 
 const app = express();
 
@@ -13,8 +13,41 @@ const connection = mysql.createConnection({
     database: 'employee_DB',
 });
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log(`connected as id ${connection.threadId}`);
-    connection.end();
-});
+    connection.connect((err) => {
+        if (err) throw err;
+        console.log(`connected as id ${connection.threadId}`);
+        connection.end();
+    });
+
+const vamonos = () => {
+    inquirer.prompt({
+        name: 'enter',
+        type:'list',
+        message: 'What would you like to do?',
+        choices: ['add department', 'add roles', 'add employees', 'view department',
+         'view roles', 'view employees', 'update employee roles'],
+    })
+    .then((answers) => {
+        switch (answer.enter) {
+            case 'add department':
+                addDepartment();
+                break;
+
+            case 'add roles':
+                addEployees();
+                break;
+
+            case 'view department':
+                viewDepartment();
+                break;
+
+            case 'update employee roles':
+                updateEmployeeRoles();
+                break;
+
+            default:
+                console.log(`error what you enter is not valid: ${answer.enter}`);
+                break;
+        }
+    });
+};
