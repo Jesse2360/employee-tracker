dc8e13a2ee941b078c6c15f5d735737413ae0d87
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-05-22 01:22:59 -0500

    add question in inquirer to department & employees

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const connection = mysql.createConnection({
         if (err) throw err;
         console.log(`connected as id ${connection.threadId}`);
         connection.end();
+        vamonos();
     });
 
 const vamonos = () => {
@@ -34,7 +35,7 @@ const vamonos = () => {
                 break;
 
             case 'add roles':
-                addEployees();
+                addEmployees();
                 break;
 
             case 'view department':
@@ -49,5 +50,67 @@ const vamonos = () => {
                 console.log(`error what you enter is not valid: ${answer.enter}`);
                 break;
         }
+    });
+};
+
+const addDepartment = () => {
+    inquirer.prompt([
+    {
+        name: 'department_name',
+        type: 'input',
+        message: 'please enter department name',
+    },
+    ])
+    .then((answer) => {
+        connection.query('INSERT INTO department SET?',
+            {
+                name: answer.department_name,
+            },
+            (err) => {
+                if (err) throw err;
+                console.log('added name success');
+                vamonos();
+            } 
+        );
+    });
+};
+
+const addEmployees = () => {
+    inquirer.prompt([
+        {
+            name: 'first_name',
+            type: 'input',
+            message: 'please enter employee first name',
+        },
+        {
+            name: 'last_name',
+            type: 'input',
+            message: 'please enter employee last name',
+        },
+        {
+            name: 'role_id',
+            type: 'input',
+            message: 'please enter employee role id',
+        },
+        {
+            name: 'manager_id',
+            type: 'input',
+            message: 'please enter manager id',
+        },
+    ])
+    .then((answer) => {
+        connection.query('INSERT INTO employees SET ?',
+            {
+                first_name: answer.first_name,
+                last_name: answer.last_name,
+                role_id: answer.role_id,
+                manager_id: answer.manager_id,
+            },
+            (err) => {
+                if (err) throw err;
+                console.log('added employeee success');
+                vamonos();
+            }  
+        );
     });
 };
